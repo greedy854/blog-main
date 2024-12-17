170c9478396b85559ae1ac8cab2640426f74adb3
Greedy <34904@ma-web.nl>
2024-12-17 20:08:16 +0100

    Fix fs module issue for Heroku deployment

--- config-overrides.js
+++ config-overrides.js
@@ -3,9 +3,10 @@ const path = require('path');
 module.exports = function override(config) {
   config.resolve.fallback = {
     ...config.resolve.fallback,
+    fs: false, // Add this line to avoid using 'fs' in browser build
     path: require.resolve('path-browserify'),
     net: require.resolve('net-browserify'),
-    http: require.resolve('stream-http') // Add this line for 'http' polyfill
+    http: require.resolve('stream-http')
   };
   return config;
 };
